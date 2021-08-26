bd359989a4ce0f003988a3e6a6cd605636c0d693
Draco Metallium (Rodrigo S. Cañibano) <draco.kun@gmail.com>
2021-08-26 17:23:12 -0300

    Estableciendo estado activo como default

--- lib/hospital.ex
+++ lib/hospital.ex
@@ -494,7 +494,7 @@ defmodule Hospital.UsuarioHospital do
     field(:idHospital, :string, primary_key: true)
     field(:cuil, :string, primary_key: true)
     field(:idRol, :integer, primary_key: true)
-    field(:estadoLaboral, :integer)
+    field(:estadoLaboral, :integer, default: 0)
   end
 end
 
@@ -532,7 +532,7 @@ defmodule Hospital.UsuarioSector do
     field(:idHospital, :string, primary_key: true)
     field(:idSector, :string, primary_key: true)
     field(:cuil, :string)
-    field(:estado, :integer)
+    field(:estado, :integer, default: 0)
   end
 end
 
@@ -549,6 +549,6 @@ defmodule Hospital.Cama do
     field(:ubicacionX, :integer)
     field(:ubicacionY, :integer)
     field(:orientacion, :string)
-    field(:estado, :integer)
+    field(:estado, :integer, default: 0)
   end
 end
