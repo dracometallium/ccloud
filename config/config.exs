cb057b16ee3cbcfb23f28ef89da408830df9cced
Draco Metallium (Rodrigo S. Cañibano) <draco.kun@gmail.com>
2022-02-10 11:59:21 -0300

    Agregando fecha a los logs

--- config/config.exs
+++ config/config.exs
@@ -13,6 +13,10 @@ config :logger,
   level: :debug,
   backends: [:console, {LoggerFileBackend, :file_log}]
 
+config :logger, :console,
+  format: "[$date] [$time] [$level] $message\n"
+
 config :logger, :file_log,
   path: "myLog.log",
+  format: "[$date] [$time] [$level] $message\n",
   level: :debug
